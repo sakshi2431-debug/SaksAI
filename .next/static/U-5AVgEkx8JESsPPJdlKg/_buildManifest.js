526897235b62300f9e4439b46fff89ca8d175fe1
Sakshi Rane <76606602+sakshi2431-debug@users.noreply.github.com>
2026-02-10 04:50:10 +0000

    Add chat page

---
+++ .next/static/U-5AVgEkx8JESsPPJdlKg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/chat":["static/chunks/pages/chat-f694c5d2463a683b.js"],sortedPages:["/_app","/_error","/chat"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
